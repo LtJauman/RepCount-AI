52bf264ad3f206d920c0dc88d05fddf5e85498aa
djaumandreuonda <diego.jaumandreuonda@ada.ac.uk>
2023-03-18 20:13:23 +0000

    Loading images and sending them to model
    results don't work properly though, creaint this in case I need to go back to a previous version

--- model/services.js
+++ model/services.js
@@ -4,36 +4,75 @@
 
 //for easy lets setup some quick global variables
 var imageModelURL = 'https://teachablemachine.withgoogle.com/models/VUHqJPevq/'; //variable used to hold path to the model
+// Image: https://teachablemachine.withgoogle.com/models/VUHqJPevq/
+// My pose model: https://teachablemachine.withgoogle.com/models/iPqH_pPou/
 var classifier; //variable used to hold the classifier object
 
 var cam; //variable used to hold the camera object
 var label0 = "", confidence0 = 0; //for ease and just because we're only demo'ing with two classes
 var label1 = "", confidence1 = 0;
+let myimage;
+let loadedImages = [];
 
 function preload() {
 	//p5 function - this function is automatically called by the p5 library, once only
 	classifier = ml5.imageClassifier(imageModelURL + 'model.json'); //load the model!
+	myimage = loadImage("B/37putput_0001.png");
+	const imageList = [
+		"B/37putput_0001.png",
+		"B/37putput_0002.png",
+		"B/37putput_0003.png",
+	  ];
+	  for (let i = 0; i <= imageList.length -1 ; i++) {
+		console.log(imageList[i])
+		let img = loadImage(imageList[i]);
+		loadedImages.push(img);
+	  }
+	  console.log(loadedImages)
+	  requestresults(loadedImages);
+	// let imageList = loadStrings('B');
+	// let images; 
+	// // loop through the list to load each image
+	// for (let i = 0; i < imageList.length; i++) {
+	// 	console.log("here")
+	// 	console.log(imageList[i])
+	//   // load each image and add it to the images array
+	//   let imagePath = 'B/' + imageList[i];
+	//   let image = loadImage(imagePath);
+	//   images.push(image);
+	// }
+
 }
 
 
 function setup() {
 	//p5 function - this function is autmaticallt called after the 'preload' function; the function is only executed once
 	var viewport = createCanvas(480, 360);//p5 function to create a p5 canvas 
-	viewport.parent('video_container'); //attach the p5 canvas to the target html div
-	frameRate(24); //set the frame rate, we dont need to high performance video
+	//viewport.parent('video_container'); //attach the p5 canvas to the target html div
+	//frameRate(24); //set the frame rate, we dont need to high performance video
 
-	cam = createCapture(VIDEO);//p5 function, store the video information coming from the camera
-	cam.hide();//hide the cam element
-
-	classify(); //start the classifer
-	getImages();
+	//cam = createCapture(VIDEO);//p5 function, store the video information coming from the camera
+	//cam.hide();//hide the cam element
+	
+	//classify(); //start the classifer
+	//getImages();
 	//getImagesByFolderDir("C:/Users/User/Videos/Captures/Pull_ups/Testing data/B");
 }
 
 
 function classify() {
 	//ml5, classify the current information stored in the camera object
-	classifier.classify(cam, processresults); //once complete execute a callback to the processresults function
+	//let image = createImg("C:\\Users\\User\\Documents\\GitHub\\RepCount-AI\\model\\B\\37putput_0120.png", "a picture")
+	let img = 
+	classifier.classify(myimage, processresults); //once complete execute a callback to the processresults function
+	console.log(friendlyresults())
+}
+
+function requestresults(images){
+	for(let i = 0; i <= images.length -1 ; i++){
+		classifier.classify(images[i], processresults);
+	}
+	console.log(label0)
 }
 
 
@@ -60,58 +99,10 @@ function friendlyresults() {
 
 
 function draw() {
+	//console.log(friendlyresults())
 	//p5 function - this function is automatically called every frame
 	background("#c0c0c0"); //set the canvas default back colour
 
-	image(cam, 0, 0); //pass the video to the p5 canvas
-	document.getElementById("results").innerHTML = friendlyresults(); //update the result string
+	//image(cam, 0, 0); //pass the video to the p5 canvas
+	//document.getElementById("results").innerHTML = friendlyresults(); //update the result string
 }
-
-
-function getImages() {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', 'C:/Users/User/Videos/Captures/Pull_ups/Testing data/B');
-	xhr.onload = function() {
-	  if (xhr.status === 200) {
-		var fileNames = [];
-		var files = xhr.responseXML.getElementsByTagName('a');
-		for (var i = 0; i < files.length; i++) {
-		  var fileName = files[i].textContent;
-		  if (fileName.endsWith('.png')) {
-			fileNames.push(fileName);
-		  }
-		}
-		console.log(fileNames);
-	  }
-	};
-	xhr.send();
-  }
-  
-function getImagesByFolderDir(folderDir) {
-	const folderPath = path.join(__dirname, folderDir);
-	const images = [];
-  
-	fs.readdirSync(folderPath).forEach(file => {
-	  images.push(file);
-	});
-  
-	console.log(images);
-  }
-
-const fs = require('fs');
-const path = require('path');
-
-function getImagesByFolderDir(folderDir) {
-  const folderPath = path.join(__dirname, folderDir);
-  const images = [];
-
-  fs.readdirSync(folderPath).forEach(file => {
-    images.push(file);
-  });
-
-  return images;
-}
-
-// Example usage:
-const images = getImagesByFolderDir('all_pictures');
-console.log(images); // array of image file names in the "all_pictures" directory
